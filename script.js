e887970c5d223d6d6cf4b701e708bfe938708896
Amartya Pandey <pandeyamartya@outlook.com>
2025-01-07 20:39:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Add the right passwords for validation
 const validpass1 = '';
 const validpass2 = '';
 const validpass3 = '';
@@ -8,7 +9,6 @@ event.preventDefault();
 
 const pass = document.getElementById('pass').value.toLowerCase();
 
-// Validate the credentials
 if (pass === validpass1 || pass === validpass2 || pass === validpass3 || pass === validpass4) {
     alert('You got it right');
     window.location.href = 'surprise_page.html';
@@ -16,3 +16,7 @@ if (pass === validpass1 || pass === validpass2 || pass === validpass3 || pass ==
     document.getElementById('error-message').style.display = 'block';
 }
 });
+
+
+// If you want to remove the password's use 
+// interchange the logic inside if and else block.
